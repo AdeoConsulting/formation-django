6e13462ed513c0d41e269f226ed1513a4680f1a1
Stephcyrille <stephcyril.sc@gmail.com>
2022-10-19 20:15:02 +0200

    Redifine repo base structure

--- academica/academica/urls.py
+++ academica/academica/urls.py
@@ -1,4 +1,4 @@
-"""lesson1 URL Configuration
+"""academica URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
